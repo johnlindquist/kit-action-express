286c3cdcca91b3bb3a183c95b2fd2f5e57de1e2d
John Lindquist <johnlindquist@gmail.com>
2022-11-02 17:01:11 -0600

    fix(hi): hi

--- scripts/tunnel.js
+++ scripts/tunnel.js
@@ -1,4 +1,4 @@
 let port = +(await arg("Port:"))
-let ngrok = await npm("ngrok")
-let url = await ngrok.connect(+port)
-console.log(url)
+let localtunnel = await npm("localtunnel")
+let tunnel = await localtunnel({ port: 3000 })
+console.log(tunnel.url)
