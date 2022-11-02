a37a4d784d984ce206a2a62104eced03ef58fac0
John Lindquist <johnlindquist@gmail.com>
2022-11-02 16:09:50 -0600

    fix(hi): hi

--- scripts/server.js
+++ scripts/server.js
@@ -1,11 +1,14 @@
 let express = await npm("express")
 
 let app = express()
+let port = await detectPort(3000)
+
+let { ip } = await $`dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com`
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.listen(3000, () => {
-  console.log("Server started")
+app.listen(port, () => {
+  console.log(`Example app listening at http://${ip}:${port}`)
 })
