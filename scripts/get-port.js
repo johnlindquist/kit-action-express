08bc2286608271431eed7565ae718c17ca82a5de
John Lindquist <johnlindquist@gmail.com>
2022-11-02 16:37:26 -0600

    fix(hi): hi

--- scripts/get-port.js
+++
@@ -1,3 +0,0 @@
-let ngrok = await npm("ngrok")
-let url = await ngrok.connect(+(await arg("Enter port")))
-console.log(url)
--- scripts/server.js
+++ scripts/server.js
@@ -1,5 +1,7 @@
 let port = await arg("Enter port")
 
+let core = await npm("@actions/core")
+let ngrok = await npm("ngrok")
 let express = await npm("express")
 let app = express()
 
@@ -7,6 +9,9 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.listen(+port, () => {
-  console.log(`Example app listening at ${port}`)
+app.listen(+port, async () => {
+  core.debug(`Example app listening at ${port}`)
+
+  let url = await ngrok.connect(+port)
+  core.debug(url)
 })
