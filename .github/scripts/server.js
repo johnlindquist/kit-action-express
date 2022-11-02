dabf521d7af86f8ec381dc5ea4a8ea164db71b3b
John Lindquist <johnlindquist@gmail.com>
2022-11-02 15:54:06 -0600

    hi

---
+++ .github/scripts/server.js
@@ -0,0 +1,11 @@
+let express = await npm("express")
+
+let app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+  console.log("Server started")
+})
