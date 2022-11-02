e3d8f36df83c3c21e6645aa913ad12a25f5a5e38
John Lindquist <johnlindquist@gmail.com>
2022-11-02 16:38:43 -0600

    fix(hi): hi

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,12 @@
 let port = await arg("Enter port")
 
 let core = await npm("@actions/core")
+
+core.debug(`This is a debug message`)
+core.info(`This is an info message`)
+core.warning(`This is a warning message`)
+core.notice(`This is a notice message`)
+
 let ngrok = await npm("ngrok")
 let express = await npm("express")
 let app = express()
